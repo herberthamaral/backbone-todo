51fea110d24a78c9ac444d9d92ab9350a74c4d2a
Herberth Amaral <herberthamaral@gmail.com>
2011-11-19 07:27:58 -0200

    primeira versão funcional da app

--- myapp.js
+++ myapp.js
@@ -1,20 +1,50 @@
-//ToDO list
-// Lista de Todos
-// Todo
-//
 $(document).ready(function(){
     window.Todo = Backbone.Model.extend({
         defaults:{
             todo:'',
-            visible_status:'visible',
             is_done: false
+        }
+    });
+
+    window.Todos = Backbone.Collection.extend({
+        model: window.Todo,
+        url: '/albums',
+        remainingItems: function(){
+            return this.filter(function(item){ 
+                if (!item.get('is_done'))
+                    return item;
+            });
+        }
+    });
+    
+    window.EnterTodoView = Backbone.View.extend({
+        events:{
+            'keypress #todo-text': 'keypress'
+        },
+        initialize: function(){
+            _.bindAll(this, 'render');
+        },
+        render:function(){
+            var renderedContent = _.template($('#todo-enter-view').html());    
+            $(this.el).html(renderedContent);
+            return this;
+        },
+        keypress: function(ev){
+            console.log(ev);
+            el = $(this.el).find('#todo-text')
+            if (ev.originalEvent.keyCode==13){
+                var todo = new Todo({todo:el.val()});
+                window.todos.add(todo);
+                var todoView = new TodoView({model:todo});
+                $('#container').append(todoView.render().el);
+                $(el).val('').blur();
+            }
         }
     });
 
     window.TodoView = Backbone.View.extend({
         events:{
-            'click .remove': 'remove',
-            'change checkbox': 'check_done'
+            'change .done_status': 'change_done_status'
         },
 
         initialize: function(){
@@ -27,17 +57,35 @@ $(document).ready(function(){
             $(this.el).html(renderedContent);
             return this;
         },
-        remove:function(){
-            this.model.set({visible_status:'hidden'});
+        change_done_status:function(){
+            this.model.set({is_done:!this.model.get('is_done')});
+        }
+    });
+
+    window.RemainingItemsView = Backbone.View.extend({
+        initialize: function(){
+            _.bindAll(this, 'render');
+            window.todos.bind('change', this.render);
+            window.todos.bind('add', this.render);
+            this.template = _.template($('#remaining-items-view').html());
         },
-        check_done:function(){
-            $(this.el).find('checkbox').is('checked');
+        render:function(){
+            var remaining = window.todos.remainingItems().length;
+            var renderedContent = this.template({remaining: remaining});
+            $(this.el).html(renderedContent);
+            return this;
         }
     });
 
     window.todo = new Todo({todo: 'Ir ao banco'});
     window.todoView = new TodoView({model: todo});
-    $('#container').html(todoView.render().el);
+    window.todos = new Todos();
+    window.entertodoview = new EnterTodoView();
+    window.remainingItemsView = new RemainingItemsView();
+    todos.add(todo);
+    $('#container').append(entertodoview.render().el);
+    $('#container').append(todoView.render().el);
+    $('#counter').append(remainingItemsView.render().el);
 
 });
 
